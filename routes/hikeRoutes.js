2ca5d8a913a44cb8d9b0657130b1de462f865483
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-03-17 21:15:04 -0600

    Everuthing moved from Project3 folder

---
+++ routes/hikeRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const hikeController = require("../controllers/hikeController");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(hikeController.getAllHikes)
+  .post(hikeController.createHike);
+
+router
+  .route("/:id")
+  .get(hikeController.getHike)
+  .patch(hikeController.updateHike)
+  .delete(hikeController.deleteHike);
+
+module.exports = router;
