9133ffcfc3f9ed269f347bfd8c87d70aa61099bd
DallinRodgers <rodgers.dallin@gmail.com>
2020-03-19 17:21:12 -0600

    Edit model description

--- models/hikeModel.js
+++ models/hikeModel.js
@@ -25,7 +25,6 @@ const hikeSchema = new mongoose.Schema({
   },
   description: {
     type: [String],
-    required: [true, "Needs a description"],
     trim: true,
     default: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum leo vel orci porta. Urna molestie at elementum eu facilisis. Volutpat odio facilisis mauris sit amet massa vitae. Sodales neque sodales ut etiam sit amet nisl. Nec tincidunt praesent semper feugiat nibh. Fusce id velit ut tortor pretium viverra suspendisse potenti.", "Sed egestas egestas fringilla phasellus faucibus. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Sit amet aliquam id diam. Amet tellus cras adipiscing enim eu turpis egestas pretium aenean. Ac turpis egestas maecenas pharetra convallis. Sit amet justo donec enim diam vulputate ut pharetra sit. Eu non diam phasellus vestibulum lorem. Dis parturient montes nascetur ridiculus mus mauris vitae. Augue neque gravida in fermentum et sollicitudin. Et netus et malesuada fames ac turpis."]
   },
