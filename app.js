e74f164b7c9c092967d39694b3cd6f6c9b099df0
DallinRodgers <rodgers.dallin@gmail.com>
2020-03-19 16:21:12 -0600

    Add node script

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware 👋");
+  console.log("Hello from the middleware");
   next();
 });
 
