89e262c9918d351056c5f81a4d1d7c71ceb1fcfa
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-03-19 20:37:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ const app = express();
 // 1) MIDDLEWARES
 
 app.use(express.json());
-// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from the middleware");
   next();
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 // 3) ROUTES
 app.use("/api/v1/hikes", hikeRouter);
