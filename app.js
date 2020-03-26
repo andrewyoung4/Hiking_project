293a09a075b772b24de7ad19155ab5c80bd1a896
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-03-26 09:23:35 -0600

    Update app.js
    created route for undefined routes

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log("Hello from the middleware");
-  next();
-});
-
 // app.use((req, res, next) => {
 //   req.requestTime = new Date().toISOString();
 //   next();
@@ -25,5 +20,12 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use("/api/v1/hikes", hikeRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    static: "fail",
+    message: `Cannot find ${req.originalUrl} on this server!`
+  });
+});
 
 module.exports = app;
