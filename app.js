ef616bd18b995b0f41d05166dcddef4f78019ad2
DallinRodgers <rodgers.dallin@gmail.com>
2020-03-26 15:57:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -22,9 +22,27 @@ app.use("/api/v1/hikes", hikeRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    static: "fail",
-    message: `Cannot find ${req.originalUrl} on this server!`
+  // res.status(404).json({
+  //   static: "fail",
+  //   message: `Cannot find ${req.originalUrl} on this server!`
+  // });
+
+  const err = new Error(`Cannot find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // When something is passed through next() express treats it as an error
+  next(err);
+});
+
+// With four parameters express knows this is an error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
